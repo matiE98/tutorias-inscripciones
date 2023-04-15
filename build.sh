2b9a90e68570238eaacf92efb1433e56f98fe254
Evin Matias <matiasevin98@gmail.com>
2023-04-15 07:58:32 -0300

    makemigrations inscriptos

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations Alumno
+python manage.py makemigrations inscriptos
 python manage.py migrate
