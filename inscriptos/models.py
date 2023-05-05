349a19956d1b0ed9eb94bbf019d31125e50d05f9
Evin Matias <matiasevin98@gmail.com>
2023-05-05 14:51:54 -0300

    lista de preinscriptos subida a la db

--- inscriptos/models.py
+++ inscriptos/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Alumno(models.Model):
     dni = models.IntegerField(primary_key=True)
+    apellido = models.CharField(max_length=250, null=False, default='sin_apellido')
     nombre = models.CharField(max_length=250, null=False, default='sin_nombre')
-    carrera = models.CharField(max_length=5, null=False, default='sin_carrera')
+    carrera = models.CharField(max_length=30, null=False, default='sin_carrera')
 
     def __str__(self):
         return self.dni
